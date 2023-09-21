fae35f5f114a46af4e138f2f70f6dd00f51ae67c
Iamneo <techadmin@iamneo.ai>
2023-09-21 16:54:18 +0000

    Backup Commit

--- terraform/script.tf
+++ terraform/script.tf
@@ -1,6 +1,10 @@
 provider "aws"
 {
     region="ap-northeast-1"
-    access_key=""
-    secret_key=""
+    access_key="AKIA2SACZ4LTCOJLBWDX"
+    secret_key="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"
+}
+resource "aws_instance" "ec2_instance"
+{
+    ami=""
 }
